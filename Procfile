f88d9484021280f2b01e09b460923ed85bc3987f
tuenguyen <tue.nguyen.2411@gmail.com>
2021-03-28 19:23:25 +0700

    fix time

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn spoj.wsgi --timeout 1000 --log-file -
-worker: python3 update_bot.py
--- update_bot.py
+++ update_bot.py
@@ -7,6 +7,5 @@ import logging
 if __name__ == '__main__':
     prev_time = time.time()
     while True:
-        if time.time() - prev_time > 300:
-            prev_time = time.time()
-            r = requests.get("http://spoj-tour.herokuapp.com/update_all_user")
+        time.sleep(300)
+        r = requests.get("http://spoj-tour.herokuapp.com/update_all_user")
